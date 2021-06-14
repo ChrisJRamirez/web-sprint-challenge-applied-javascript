878bfd86ab34121ddc9c37f14cccf05f8dcad090
ChrisJRamirez <chrisjamesramirez@gmail.com>
2021-06-13 20:12:32 -0400

    tests 1 through 4 passing, done with task 1 and 2

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,50 @@
+// TASK 1
+// ---------------------
+// Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
+// The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+// The text inside elements will be set using their `textContent` property (NOT `innerText`).
+//
+//  <div class="header">
+//    <span class="date">{ date }</span>
+//    <h1>{ title }</h1>
+//    <span class="temp">{ temp }</span>
+//  </div>
+//
+
 const Header = (title, date, temp) => {
-  // TASK 1
-  // ---------------------
-  // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  //  <div class="header">
-  //    <span class="date">{ date }</span>
-  //    <h1>{ title }</h1>
-  //    <span class="temp">{ temp }</span>
-  //  </div>
-  //
+  const headerTag = document.createElement("div")
+  const titleTag = document.createElement("h1")
+  const dateTag = document.createElement("span")
+  const tempTag = document.createElement("span")
+
+  headerTag.classList.add("header")
+  dateTag.classList.add("date")
+  tempTag.classList.add("temp")
+  dateTag.textContent = date
+  titleTag.textContent = title
+  tempTag.textContent = temp
+
+  headerTag.appendChild(dateTag)
+  headerTag.appendChild(titleTag)
+  headerTag.appendChild(tempTag)
+
+  return headerTag
 }
 
-//setting up codegrade test
+
+
 
 const headerAppender = (selector) => {
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
   // It should create a header using the Header component above, passing arguments of your choosing.
+  const header = Header(selector.title, selector.date, selector.temp)
   // It should append the header to the element in the DOM that matches the given selector.
-  //
+
+  const domSelector = document.querySelector(selector)
+  domSelector.appendChild(header)
+
 }
 
 export { Header, headerAppender }
